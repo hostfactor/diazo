98b3342c7fefaafb7a26977019303d695d53cde6
edwardowens <eddiebeme@gmail.com>
2022-03-13 22:06:12 -0700

    feat(fileselect): Adding fileselect funcs.

--- pkg/fileselect/public.go
+++ pkg/fileselect/public.go
@@ -29,3 +29,37 @@ func FindFileSelection(name string, fs []*filesystem.FileSelection) (*filesystem
 	}
 	return nil, -1
 }
+
+// GetLastFilename gets the last filename from the list of filesystem.FileLocation. The filename will only consist of
+// the name and the extension e.g. "save.zip".
+func GetLastFilename(locs []*filesystem.FileLocation) string {
+	if len(locs) == 0 {
+		return ""
+	}
+
+	return GetLocationFilename(locs[len(locs)-1])
+}
+
+// GetFilenames gathers all filenames from a slice of locations. The filenames will only consist of the name and the extension e.g.
+// "save.zip".
+func GetFilenames(fs []*filesystem.FileLocation) []string {
+	out := make([]string, 0, len(fs))
+	for _, v := range fs {
+		name := GetLocationFilename(v)
+		if name != "" {
+			out = append(out, name)
+		}
+	}
+
+	return out
+}
+
+// GetLocationFilename retrieves the filename from the filesystem.FileLocation. The filename will consist of the name
+// and the extension e.g. "save.zip".
+func GetLocationFilename(loc *filesystem.FileLocation) string {
+	if f := loc.GetBucketFile(); f != nil {
+		return f.Name
+	}
+
+	return ""
+}
